6f800dae9209a0f87ec7e1750d3f4f31b7dcee03
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-12 01:43:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(people)
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-    people.each_with_index(1) do |person, index|
+    people.each.with_index(1) do |person, index|
       current_line << " #{index}. #{person}"
     end
     puts current_line
